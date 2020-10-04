d6750c139ec9b56219117a42770b295daa09bc04
Fatma Hameed <fatma78hameed@gmail.com>
2020-10-04 01:17:02 -0700

    deleting unnecessary comments

--- week2/homework/exercises/1-blog-API/server.js
+++ week2/homework/exercises/1-blog-API/server.js
@@ -7,10 +7,7 @@ app.use(express.json());
 // YOUR CODE GOES IN HERE
 // 1- create a blog with title and content
 app.post('/blogs', (req, res) => {
-  // How to get the title and content from the request??
-
   let newBlog = {
-    // id: id,
     title: req.body.title,
     content: req.body.content,
   };
@@ -18,19 +15,11 @@ app.post('/blogs', (req, res) => {
   fs.writeFileSync(newBlog.title, newBlog.content);
   res.end('ok');
 });
-// app.get('/', function (req, res) {
-//   res.send('Hello World')
-// })
 
 // 2- Update the post
 
 app.put('/blogs/:title', (req, res) => {
-  // How to get the title and content from the request?
-  // What if the request does not have a title and/or content?
-  // console.log(req.body.title);
-  // console.log(req.params.title);
   if (fs.existsSync(req.params.title)) {
-    // console.log(req.params.title);
     fs.writeFileSync(req.body.title, req.body.content);
     res.end('ok');
   } else {
@@ -41,7 +30,6 @@ app.put('/blogs/:title', (req, res) => {
 });
 //3- Delete blogs
 app.delete('/blogs/:title', (req, res) => {
-  // How to get the title from the url parameters?
   if (fs.existsSync(req.params.title)) {
     // Add condition here
     fs.unlinkSync(req.params.title);
@@ -55,7 +43,6 @@ app.delete('/blogs/:title', (req, res) => {
 
 //Reading blog
 app.get('/blogs/:title', (req, res) => {
-  // How to get the title from the url parameters?
   // check if post exists
   const post = fs.readFileSync(req.params.title);
   if (fs.existsSync(req.params.title)) {
@@ -64,6 +51,5 @@ app.get('/blogs/:title', (req, res) => {
     res.status(404);
     res.send('This post does not exist!');
   }
-  // send response
 });
 app.listen(3000);
