c7d7da349315d06b2eef9a35bcf21753b626d355
Fatma Hameed <fatma78hameed@gmail.com>
2020-10-05 05:59:21 -0700

    added some comments

--- week2/homework/exercises/1-blog-API/server.js
+++ week2/homework/exercises/1-blog-API/server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
-
+// require file system packages
 const fs = require('fs');
+
+// Parse the json data
 app.use(express.json());
 
-// YOUR CODE GOES IN HERE
 // 1- create a blog with title and content
 app.post('/blogs', (req, res) => {
   let newBlog = {
@@ -19,37 +20,40 @@ app.post('/blogs', (req, res) => {
 // 2- Update the post
 
 app.put('/blogs/:title', (req, res) => {
+  // put conditionality if the file is exist, or the parameter of the url is correct
   if (fs.existsSync(req.params.title)) {
     fs.writeFileSync(req.body.title, req.body.content);
     res.end('ok');
   } else {
     // Send response with error message
     res.status(404);
-    res.send('This post does not exist!');
+    res.end('This blog does not exist!');
   }
 });
 //3- Delete blogs
 app.delete('/blogs/:title', (req, res) => {
+  // Add condition here
   if (fs.existsSync(req.params.title)) {
-    // Add condition here
     fs.unlinkSync(req.params.title);
     res.end('ok');
   } else {
     // Respond with message here
     res.status(404);
-    res.send('This post does not exist!');
+    res.end('This blog does not exist!');
   }
 });
 
 //Reading blog
 app.get('/blogs/:title', (req, res) => {
-  // check if post exists
-  const post = fs.readFileSync(req.params.title);
+  //store the existing blog to variable
+  const blog = fs.readFileSync(req.params.title);
+  // check if blog exists
   if (fs.existsSync(req.params.title)) {
-    res.send(post);
+    //send the
+    res.send(blog);
   } else {
     res.status(404);
-    res.send('This post does not exist!');
+    res.end('This blog does not exist!');
   }
 });
 app.listen(3000);
--- week2/homework/hackyourtemperature/server.js
+++ week2/homework/hackyourtemperature/server.js
@@ -1,17 +1,27 @@
 const express = require('express');
 const app = express();
+// require handlebars
 const exphbs = require('express-handlebars');
+// set the engine to handlebar in the view folder
 app.set('view engine', 'handlebars');
-app.engine('handlebars', exphbs({ defaultLayout: false }));
+app.engine('handlebars', exphbs({ defaultLayout: false })); // default layout is false which means that there is one folder contains one handlebar(not certain handlebars in nested folders)
+
+// rendering index handlebar in '/' endpoint
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+// parse the data to json and encode the form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+//create a city name from the request to the '/weather' end point
 app.post('/weather', (req, res) => {
   const cityName = req.body.cityName;
-  res.json(cityName);
-  // res.send(cityName);
+  // res.json(cityName);
+  res.end(cityName);
 });
 const PORT = process.env.PORT || 3000;
+
+// listen to the port
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
